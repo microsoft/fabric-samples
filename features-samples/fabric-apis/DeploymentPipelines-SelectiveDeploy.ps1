2a1a0234dabe98a09eac2c6edf8b2214bba30a05
PierreCardo <78280366+PierreCardo@users.noreply.github.com>
2025-03-10 15:38:22 +0200

    Revert "Add service principal support for CI-CD samples"

--- features-samples/fabric-apis/DeploymentPipelines-SelectiveDeploy.ps1
+++ features-samples/fabric-apis/DeploymentPipelines-SelectiveDeploy.ps1
@@ -27,13 +27,6 @@ $sourceItem = @{
     itemType = "<SOURCE ITEM TYPE>"                         # The type of the item to be deployed (e.g. Dashboard)
 }
 
-$principalType = "<PRINCIPAL TYPE>" # Choose either "UserPrincipal" or "ServicePrincipal"
-
-# Relevant for ServicePrincipal
-$clientId = "<CLIENT ID>"                   #The application (client) ID of the service principal
-$tenantId = "<TENANT ID>"                   #The directory (tenant) ID of the service principal
-$servicePrincipalSecret = "<SECRET VALUE>"  #The secret value of the service principal
-
 # End Parameters =======================================
 
 $global:baseUrl = "<Base URL>" # Replace with environment-specific base URL. For example: "https://api.fabric.microsoft.com/v1"
@@ -43,55 +36,16 @@ $global:resourceUrl = "https://api.fabric.microsoft.com"
 $global:fabricHeaders = @{}
 
 function SetFabricHeaders() {
-    if ($principalType -eq "UserPrincipal") {
-        $secureFabricToken = GetSecureTokenForUserPrincipal
-    } elseif ($principalType -eq "ServicePrincipal") {
-        $secureFabricToken = GetSecureTokenForServicePrincipal
-
-    } else {
-        throw "Invalid principal type. Please choose either 'UserPrincipal' or 'ServicePrincipal'."
-    }
-
-    # Convert SecureString to plain text
-    $fabricToken = ConvertSecureStringToPlainText($secureFabricToken)
-
-    $global:fabricHeaders = @{
-        'Content-Type' = "application/json"
-        'Authorization' = "Bearer $fabricToken"
-    }
-}
-
-function GetSecureTokenForUserPrincipal() {
-    #Login to Azure interactively
+    # Login to Azure
     Connect-AzAccount | Out-Null
 
     # Get authentication
-    $secureFabricToken = (Get-AzAccessToken -AsSecureString -ResourceUrl $global:resourceUrl).Token
+    $fabricToken = (Get-AzAccessToken -ResourceUrl $global:resourceUrl).Token
 
-    return $secureFabricToken
-}
-
-function GetSecureTokenForServicePrincipal() {
-    $secureServicePrincipalSecret  = ConvertTo-SecureString -String $servicePrincipalSecret -AsPlainText -Force
-    $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $secureServicePrincipalSecret
-
-    #Login to Azure using service principal
-    Connect-AzAccount -ServicePrincipal -TenantId $tenantId -Credential $credential | Out-Null
-
-    # Get authentication
-    $secureFabricToken = (Get-AzAccessToken -AsSecureString -ResourceUrl $global:resourceUrl).Token
-    
-    return $secureFabricToken
-}
-
-function ConvertSecureStringToPlainText($secureString) {
-    $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString)
-    try {
-        $plainText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
-    } finally {
-        [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
+    $global:fabricHeaders = @{
+        'Content-Type' = "application/json"
+        'Authorization' = "Bearer {0}" -f $fabricToken
     }
-    return $plainText
 }
 
 function GetDeploymentPipelineByName($deploymentPipelineName) {
@@ -147,26 +101,15 @@ function GetDeploymentPipelineStageItemByName($itemName, $itemType, $deploymentP
 
 function GetErrorResponse($exception) {
     # Relevant only for PowerShell Core
-    # Try to fill based on ErrorDetails.Message
-    $errorResponse = $exception.ErrorDetails.Message
+    $errorResponse = $_.ErrorDetails.Message
 
-    # If still null, try based on exception.Message
     if(!$errorResponse) {
-        $errorResponse = $exception.Message
-    }
-
-    # If still null and exception.Response isn't null, try to read the response stream and fill in
-    if(!$errorResponse -and $exception.Response) {
+        # This is needed to support Windows PowerShell
         $result = $exception.Response.GetResponseStream()
         $reader = New-Object System.IO.StreamReader($result)
         $reader.BaseStream.Position = 0
         $reader.DiscardBufferedData()
-        $errorResponse = $reader.ReadToEnd()
-    }
-
-    # If all else fails, fill in generic error
-    if(!$errorResponse) {
-        $errorResponse = "An error occurred, but no detailed message is available."
+        $errorResponse = $reader.ReadToEnd();
     }
 
     return $errorResponse
--- features-samples/git-integration/GitIntegration-CommitAll.ps1
+++ features-samples/git-integration/GitIntegration-CommitAll.ps1
@@ -18,13 +18,6 @@
 $workspaceName = "<WORKSPACE NAME>"      # The name of the workspace
 $commitMessage = "<COMMIT MESSAGE>"      # The commit message
 
-$principalType = "<PRINCIPAL TYPE>" # Choose either "UserPrincipal" or "ServicePrincipal"
-
-# Relevant for ServicePrincipal
-$clientId = "<CLIENT ID>"                   #The application (client) ID of the service principal
-$tenantId = "<TENANT ID>"                   #The directory (tenant) ID of the service principal
-$servicePrincipalSecret = "<SECRET VALUE>"  #The secret value of the service principal
-
 # End Parameters =======================================
 
 $global:baseUrl = "<Base URL>" # Replace with environment-specific base URL. For example: "https://api.fabric.microsoft.com/v1"
@@ -34,60 +27,22 @@ $global:resourceUrl = "https://api.fabric.microsoft.com"
 $global:fabricHeaders = @{}
 
 function SetFabricHeaders() {
-    if ($principalType -eq "UserPrincipal") {
-        $secureFabricToken = GetSecureTokenForUserPrincipal
-    } elseif ($principalType -eq "ServicePrincipal") {
-        $secureFabricToken = GetSecureTokenForServicePrincipal
 
-    } else {
-        throw "Invalid principal type. Please choose either 'UserPrincipal' or 'ServicePrincipal'."
-    }
-
-    # Convert SecureString to plain text
-    $fabricToken = ConvertSecureStringToPlainText($secureFabricToken)
-
-    $global:fabricHeaders = @{
-        'Content-Type' = "application/json"
-        'Authorization' = "Bearer $fabricToken"
-    }
-}
-
-function GetSecureTokenForUserPrincipal() {
-    #Login to Azure interactively
+    #Login to Azure
     Connect-AzAccount | Out-Null
 
     # Get authentication
-    $secureFabricToken = (Get-AzAccessToken -AsSecureString -ResourceUrl $global:resourceUrl).Token
+    $fabricToken = (Get-AzAccessToken -ResourceUrl $global:resourceUrl).Token
 
-    return $secureFabricToken
-}
-
-function GetSecureTokenForServicePrincipal() {
-    $secureServicePrincipalSecret  = ConvertTo-SecureString -String $servicePrincipalSecret -AsPlainText -Force
-    $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $secureServicePrincipalSecret
-
-    #Login to Azure using service principal
-    Connect-AzAccount -ServicePrincipal -TenantId $tenantId -Credential $credential | Out-Null
-
-    # Get authentication
-    $secureFabricToken = (Get-AzAccessToken -AsSecureString -ResourceUrl $global:resourceUrl).Token
-    
-    return $secureFabricToken
-}
-
-function ConvertSecureStringToPlainText($secureString) {
-    $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString)
-    try {
-        $plainText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
-    } finally {
-        [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
+    $global:fabricHeaders = @{
+        'Content-Type' = "application/json"
+        'Authorization' = "Bearer {0}" -f $fabricToken
     }
-    return $plainText
 }
 
 function GetWorkspaceByName($workspaceName) {
     # Get workspaces    
-    $getWorkspacesUrl = "$global:baseUrl/workspaces"
+    $getWorkspacesUrl = "{0}/workspaces" -f $global:baseUrl
     $workspaces = (Invoke-RestMethod -Headers $global:fabricHeaders -Uri $getWorkspacesUrl -Method GET).value
 
     # Try to find the workspace by display name
@@ -98,26 +53,15 @@ function GetWorkspaceByName($workspaceName) {
 
 function GetErrorResponse($exception) {
     # Relevant only for PowerShell Core
-    # Try to fill based on ErrorDetails.Message
-    $errorResponse = $exception.ErrorDetails.Message
+    $errorResponse = $_.ErrorDetails.Message
 
-    # If still null, try based on exception.Message
     if(!$errorResponse) {
-        $errorResponse = $exception.Message
-    }
-
-    # If still null and exception.Response isn't null, try to read the response stream and fill in
-    if(!$errorResponse -and $exception.Response) {
+        # This is needed to support Windows PowerShell
         $result = $exception.Response.GetResponseStream()
         $reader = New-Object System.IO.StreamReader($result)
         $reader.BaseStream.Position = 0
         $reader.DiscardBufferedData()
-        $errorResponse = $reader.ReadToEnd()
-    }
-
-    # If all else fails, fill in generic error
-    if(!$errorResponse) {
-        $errorResponse = "An error occurred, but no detailed message is available."
+        $errorResponse = $reader.ReadToEnd();
     }
 
     return $errorResponse
@@ -137,7 +81,7 @@ try {
     # Commit to Git
     Write-Host "Committing all changes from workspace '$workspaceName' to Git."
 
-    $commitToGitUrl = "$global:baseUrl/workspaces/$($workspace.Id)/git/commitToGit"
+    $commitToGitUrl = "{0}/workspaces/{1}/git/commitToGit" -f $global:baseUrl, $workspace.Id
 
     $commitToGitBody = @{ 		
         mode = "All"
