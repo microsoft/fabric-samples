298acda1dfd6111631fc19a9bb8d029b1f6a9787
Pierre Cardo <pierrecardo@microsoft.com>
2025-03-11 07:19:44 +0200

    Support SPN

--- features-samples/git-integration/GitIntegration-UpdateFromGit.ps1
+++ features-samples/git-integration/GitIntegration-UpdateFromGit.ps1
@@ -18,6 +18,13 @@
 
 $workspaceName = "<WORKSPACE NAME>"      # The name of the workspace
 
+$principalType = "<PRINCIPAL TYPE>" # Choose either "UserPrincipal" or "ServicePrincipal"
+
+# Relevant for ServicePrincipal
+$clientId = "<CLIENT ID>"                   #The application (client) ID of the service principal
+$tenantId = "<TENANT ID>"                   #The directory (tenant) ID of the service principal
+$servicePrincipalSecret = "<SECRET VALUE>"  #The secret value of the service principal
+
 # End Parameters =======================================
 
 $global:baseUrl = "<Base URL>" # Replace with environment-specific base URL. For example: "https://api.fabric.microsoft.com/v1"
@@ -27,22 +34,60 @@ $global:resourceUrl = "https://api.fabric.microsoft.com"
 $global:fabricHeaders = @{}
 
 function SetFabricHeaders() {
+    if ($principalType -eq "UserPrincipal") {
+        $secureFabricToken = GetSecureTokenForUserPrincipal
+    } elseif ($principalType -eq "ServicePrincipal") {
+        $secureFabricToken = GetSecureTokenForServicePrincipal
 
-    #Login to Azure
-    Connect-AzAccount | Out-Null
+    } else {
+        throw "Invalid principal type. Please choose either 'UserPrincipal' or 'ServicePrincipal'."
+    }
 
-    # Get authentication
-    $fabricToken = (Get-AzAccessToken -ResourceUrl $global:resourceUrl).Token
+    # Convert SecureString to plain text
+    $fabricToken = ConvertSecureStringToPlainText($secureFabricToken)
 
     $global:fabricHeaders = @{
         'Content-Type' = "application/json"
-        'Authorization' = "Bearer {0}" -f $fabricToken
+        'Authorization' = "Bearer $fabricToken"
     }
+}
+
+function GetSecureTokenForUserPrincipal() {
+    #Login to Azure interactively
+    Connect-AzAccount | Out-Null
+
+    # Get authentication
+    $secureFabricToken = (Get-AzAccessToken -AsSecureString -ResourceUrl $global:resourceUrl).Token
+
+    return $secureFabricToken
+}
+
+function GetSecureTokenForServicePrincipal() {
+    $secureServicePrincipalSecret  = ConvertTo-SecureString -String $servicePrincipalSecret -AsPlainText -Force
+    $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $clientId, $secureServicePrincipalSecret
+
+    #Login to Azure using service principal
+    Connect-AzAccount -ServicePrincipal -TenantId $tenantId -Credential $credential | Out-Null
+
+    # Get authentication
+    $secureFabricToken = (Get-AzAccessToken -AsSecureString -ResourceUrl $global:resourceUrl).Token
+    
+    return $secureFabricToken
+}
+
+function ConvertSecureStringToPlainText($secureString) {
+    $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureString)
+    try {
+        $plainText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
+    } finally {
+        [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
+    }
+    return $plainText
 }
 
 function GetWorkspaceByName($workspaceName) {
     # Get workspaces    
-    $getWorkspacesUrl = "{0}/workspaces" -f $global:baseUrl
+    $getWorkspacesUrl = "$global:baseUrl/workspaces"
     $workspaces = (Invoke-RestMethod -Headers $global:fabricHeaders -Uri $getWorkspacesUrl -Method GET).value
 
     # Try to find the workspace by display name
@@ -53,15 +98,26 @@ function GetWorkspaceByName($workspaceName) {
 
 function GetErrorResponse($exception) {
     # Relevant only for PowerShell Core
-    $errorResponse = $_.ErrorDetails.Message
+    # Try to fill based on ErrorDetails.Message
+    $errorResponse = $exception.ErrorDetails.Message
 
+    # If still null, try based on exception.Message
     if(!$errorResponse) {
-        # This is needed to support Windows PowerShell
+        $errorResponse = $exception.Message
+    }
+
+    # If still null and exception.Response isn't null, try to read the response stream and fill in
+    if(!$errorResponse -and $exception.Response) {
         $result = $exception.Response.GetResponseStream()
         $reader = New-Object System.IO.StreamReader($result)
         $reader.BaseStream.Position = 0
         $reader.DiscardBufferedData()
-        $errorResponse = $reader.ReadToEnd();
+        $errorResponse = $reader.ReadToEnd()
+    }
+
+    # If all else fails, fill in generic error
+    if(!$errorResponse) {
+        $errorResponse = "An error occurred, but no detailed message is available."
     }
 
     return $errorResponse
@@ -81,13 +137,13 @@ try {
     # Get Status
     Write-Host "Calling GET Status REST API to construct the request body for UpdateFromGit REST API."
 
-    $gitStatusUrl = "{0}/workspaces/{1}/git/status" -f $global:baseUrl, $workspace.Id
+    $gitStatusUrl = "$global:baseUrl/workspaces/$($workspace.Id)/git/status"
     $gitStatusResponse = Invoke-RestMethod -Headers $global:fabricHeaders -Uri $gitStatusUrl -Method GET
 
     # Update from Git
     Write-Host "Updating the workspace '$workspaceName' from Git."
 
-    $updateFromGitUrl = "{0}/workspaces/{1}/git/updateFromGit" -f $global:baseUrl, $workspace.Id
+    $updateFromGitUrl = "$global:baseUrl/workspaces/$($workspace.Id)/git/updateFromGit"
 
     $updateFromGitBody = @{ 
         remoteCommitHash = $gitStatusResponse.RemoteCommitHash
